4c67bf514e2031265ed7e9006948b0f845a32548
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-07-08 22:29:59 -0300

    Nuevas modificaciones de código

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var video = document.getElementById('video-principal');
-    video.play();
-});
 
-window.addEventListener('load', function() {
-    const numPetals = 100;
-    const container = document.querySelector('.informacion-en-general');
-  
-    for (let i = 0; i < numPetals; i++) {
-      const petal = document.createElement('div');
-      petal.classList.add('rosa');
-      petal.style.setProperty('--left-pos', `${Math.random() * 100}%`);
-      petal.style.setProperty('--rotate', Math.random() * 360);
-      petal.style.setProperty('--delay', Math.random() * 5); // Retraso aleatorio entre 0 y 5 segundos
-      container.appendChild(petal);
-    }
-  });
 
   const miVideo = document.getElementById('imagen-2');
 
